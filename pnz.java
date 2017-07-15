dc1bc641183a3121c383030b167371cdc31d0283
Jegadeeswari <grjaga8@gmail.com>
2017-07-15 09:57:15 +0530

    Create pnz.java

---
+++ pnz.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class pnz
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int num=s.nextInt();
+if(num>0)
+{
+System.out.print(positive);
+}
+else
+System.out.print(negative);
+}
+}
